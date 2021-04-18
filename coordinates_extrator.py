c8ceaf31152b2adba3df012ed8462b778c91e1c3
Cameron Wonchoba <wonch002@umn.edu>
2021-04-18 17:25:12 -0500

    Adding coordinates into dataset

--- coordinates_extrator.py
+++ coordinates_extrator.py
@@ -37,6 +37,32 @@ def extract_geocode(address):
     return geocode_result[0]
 
 
+def extract_lat(geocode):
+    """Extract latitude from geocode result.
+
+    Extracts latitude from the geocode result (given by the google api).
+
+    Parameters
+    ----------
+    geocode: dict
+        Dictionary of geocode results
+    """
+    return geocode['geometry']['location']['lat']
+
+
+def extract_lng(geocode):
+    """Extract longitude from geocode result.
+
+    Extracts longitude from the geocode result (given by the google api).
+
+    Parameters
+    ----------
+    geocode: dict
+        Dictionary of geocode results
+    """
+    return geocode['geometry']['location']['lng']
+
+
 def extract_all_geocodes():
     """Extract longitude and latitude coordinates for street locations.
 
@@ -49,11 +75,19 @@ def extract_all_geocodes():
     data = pd.read_csv(LITTER_INDEX_FILE)
 
     # Truncating data for now because of cost... Serves as testing for now
-    data = data.iloc[:50]
+    data = data.iloc[:25]
 
     # Apply coordinates extractor to every row
     data['geocode_result'] = data.apply(lambda row: extract_geocode(
                     row['LR_HUNDRED_BLOCK'] + ", Philidelphia, PA"), axis=1)
+
+    # Extract latitude
+    data['lat'] = data.apply(lambda row: extract_lat(
+                    row['geocode_result']), axis=1)
+
+    # Extract longitude
+    data['lng'] = data.apply(lambda row: extract_lng(
+                    row['geocode_result']), axis=1)
 
     return data
 
